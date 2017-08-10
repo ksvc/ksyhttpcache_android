a84237d735ce091d64781d70afc69091787d9c63
xinbaicheng <xinbaicheng@kingsoft.com>
2017-08-10 10:45:37 +0800

    add javadoc

---
+++ docs/reference/lists.js
@@ -0,0 +1,10 @@
+var DATA = [
+      { id:0, label:"com.kingsoft.media.httpcache", link:"com/kingsoft/media/httpcache/package-summary.html", type:"package" },
+      { id:1, label:"com.kingsoft.media.httpcache.ErrorCode", link:"com/kingsoft/media/httpcache/ErrorCode.html", type:"class" },
+      { id:2, label:"com.kingsoft.media.httpcache.KSYProxyService", link:"com/kingsoft/media/httpcache/KSYProxyService.html", type:"class" },
+      { id:3, label:"com.kingsoft.media.httpcache.OnCacheStatusListener", link:"com/kingsoft/media/httpcache/OnCacheStatusListener.html", type:"class" },
+      { id:4, label:"com.kingsoft.media.httpcache.OnErrorListener", link:"com/kingsoft/media/httpcache/OnErrorListener.html", type:"class" },
+      { id:5, label:"com.kingsoft.media.httpcache.stats", link:"com/kingsoft/media/httpcache/stats/package-summary.html", type:"package" },
+      { id:6, label:"com.kingsoft.media.httpcache.stats.OnLogEventListener", link:"com/kingsoft/media/httpcache/stats/OnLogEventListener.html", type:"class" }
+
+    ];
